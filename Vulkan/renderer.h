afdbc297f06501a35f97706ba8b2dfb6baaf1a84
Keagan Hartman <keaganhartman@yahoo.com>
2021-10-14 16:23:24 -0400

    Moved Shaders into their own folder for easier access.

--- Vulkan/renderer.h
+++ Vulkan/renderer.h
@@ -7,130 +7,6 @@
 #ifdef _WIN32 // must use MT platform DLL libraries on windows
 	#pragma comment(lib, "shaderc_combined.lib") 
 #endif
- //Simple Vertex Shader
-#pragma region Vertex Shader
-//const char* vertexShaderSource = R"(
-//// an ultra simple hlsl vertex shader
-//#pragma pack_matrix(row_major)
-//struct OBJ_ATTR
-//{
-//	float3		Kd;				// diffuse reflectivity
-//	float			d;					// dissolve (transparency) 
-//	float3		Ks;				// specular reflectivity
-//	float			Ns;				// specular exponent
-//	float3		Ka;				// ambient reflectivity
-//	float			sharpness;	// local reflection map sharpness
-//	float3		Tf;					// transmission filter
-//	float			Ni;				// optical density (index of refraction)
-//	float3		Ke;				// emissive reflectivity
-//	float			illum;			// illumination model
-//};
-//struct SHADER_MODEL_DATA
-//{
-//	float4 lightDirection;
-//	float4 lightColor;
-//	float4 ambientLight;
-//	float4 cameraPos;
-//	float4x4 vMatrix;
-//	float4x4 pMatrix;
-//	float4x4 wMatrix[1024];
-//	OBJ_ATTR material[1024];
-//};
-//[[vk::push_constant]]
-//cbuffer MESH_INDEX {
-//	uint mesh_ID;
-//};
-//struct Vertex_Output
-//{
-//	float4 posH : SV_POSITION;
-//	float3 nrmW : NORMAL;
-//	float3 posW : WORLD;
-//};
-//struct Vertex_Input
-//{
-//	float3 pos : POSITION;
-//	float3 uvw : TEXCOORD;
-//	float3 nrm : NORMAL;
-//};
-//StructuredBuffer<SHADER_MODEL_DATA> SCENE_DATA;
-//Vertex_Output main(Vertex_Input INPUT)
-//{
-//	Vertex_Output OUTPUT;
-//	OUTPUT.posH		= float4(INPUT.pos, 1);																	// Convert Input Position into a float4 for matrix mult
-//	float4 temp_nrm = mul(float4(INPUT.nrm, 1), SCENE_DATA[0].wMatrix[mesh_ID]);	// Create Temp float4 to get get the world space normal
-//	OUTPUT.nrmW	= temp_nrm.xyz;																				// Store world space normal in float3 OUTPUT variable
-//	OUTPUT.posH		= mul(OUTPUT.posH, SCENE_DATA[0].wMatrix[mesh_ID]);			// Get world Position by multiplying world matrix
-//	OUTPUT.posW		= OUTPUT.posH.xyz;																		// Store World Position for Lighting in Float3 OUTPUT var
-//	OUTPUT.posH		= mul(OUTPUT.posH, SCENE_DATA[0].vMatrix);								// View Matrix Multiplication
-//	OUTPUT.posH		= mul(OUTPUT.posH, SCENE_DATA[0].pMatrix);								// Proj Matrix Multiplication
-//																																		
-//	return OUTPUT;
-//}
-//)";
-#pragma endregion
-
-#pragma region Pixel Shader
-// Simple Pixel Shader
-//const char* pixelShaderSource = R"(
-//// an ultra simple hlsl pixel shader
-//struct OBJ_ATTR
-//{
-//	float3		Kd;				// diffuse reflectivity
-//	float			d;					// dissolve (transparency) 
-//	float3		Ks;				// specular reflectivity
-//	float			Ns;				// specular exponent
-//	float3		Ka;				// ambient reflectivity
-//	float			sharpness;	// local reflection map sharpness
-//	float3		Tf;					// transmission filter
-//	float			Ni;				// optical density (index of refraction)
-//	float3		Ke;				// emissive reflectivity
-//	float			illum;			// illumination model
-//};
-//struct SHADER_MODEL_DATA
-//{
-//	float4 lightDirection;
-//	float4 lightColor;
-//	float4 ambientLight;
-//	float4 cameraPos;
-//	float4x4 vMatrix;
-//	float4x4 pMatrix;
-//	float4x4 wMatrix[1024];
-//	OBJ_ATTR material[1024];
-//};
-//[[vk::push_constant]]
-//cbuffer MESH_INDEX {
-//	uint mesh_ID;
-//};
-//StructuredBuffer<SHADER_MODEL_DATA> SCENE_DATA;
-//float4 main(float4 posH : SV_POSITION, float3 nrmW : NORMAL, float3 posW : WORLD) : SV_TARGET 
-//{	
-//	// Setup
-//	float4 OUTPUT;
-//	nrmW = normalize(nrmW);																								// Normalize normals beacause of Automatic LERP in Rasterizer
-//	float4 lightDirection = normalize(SCENE_DATA[0].lightDirection);									// Normalize Light Direction so DOT products in teh future are good
-//	float4 surface_color = float4(SCENE_DATA[0].material[mesh_ID].Kd, 1);						// Store Surface Color (pixel color) in an easier to access variable
-//	//			Directional Lighting
-//	float4 directionalLightRatio = saturate(dot(-lightDirection, nrmW));								// Calculate Ratio of How much Light is added to surface from Directional
-//	float4 dir_Light = surface_color * directionalLightRatio * SCENE_DATA[0].lightColor;	// surface color *  dir_light ratio * light color
-//	//			Ambient Lighting
-//	float4 ambient_Light = saturate(SCENE_DATA[0].ambientLight * surface_color);			// Get Ambient Light
-//	//			Specular Lighting
-//	float3 viewDirection = normalize(SCENE_DATA[0].cameraPos - posW);							// Get View Direction Vector by Subtracting surfPos from camPos
-//	float3 halfVector= normalize((-lightDirection) + viewDirection);									// Get the Half-Vector between viewDirection and -LightDirection
-//	float intensity;																													// Calculate Intensity
-//	intensity = saturate(dot(nrmW, halfVector));																	//		Get Ratio between Half Vector and nrmW
-//	intensity = pow(intensity, SCENE_DATA[0].material[mesh_ID].Ns);									//		Raise it to the power of Spec_Exponent
-//	intensity = max(intensity, 0);																							//		Make sure result is greater than 0
-//
-//	// intensity * specular_intensity (provided) * light color
-//	float4 spec_Light = SCENE_DATA[0].lightColor * float4(SCENE_DATA[0].material[mesh_ID].Ks, 1) * intensity;
-//	
-//	OUTPUT = saturate(dir_Light + ambient_Light + spec_Light);										// Add all Lighting together
-//	return OUTPUT;
-//}
-//)";
-#pragma endregion
-
 // Creation, Rendering & Cleanup
 class Renderer
 {
@@ -199,8 +75,8 @@ public:
 	Renderer(GW::SYSTEM::GWindow _win, GW::GRAPHICS::GVulkanSurface _vlk)
 	{
 		// Load Shader Sources
-		vertexShaderSource = ShaderAsString("../VertexShader.hlsl");
-		pixelShaderSource = ShaderAsString("../PixelShader.hlsl");
+		vertexShaderSource = ShaderAsString("../Shaders/VertexShader.hlsl");
+		pixelShaderSource = ShaderAsString("../Shaders/PixelShader.hlsl");
 	
 		win = _win;
 		vlk = _vlk;
